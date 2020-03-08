c051b0778e8dd95a8815430208290adcbb9ce716
150BigDream <2316624245@qq.com>
2020-03-08 13:47:15 +0800

    publish 页面

--- src/main/java/com/example/community/Controller/indexController.java
+++ src/main/java/com/example/community/Controller/indexController.java
@@ -19,21 +19,24 @@ public class indexController {
      * 这样子的话，重启或者什么，不还是会重新生成一个token吗？怎么识别在数据库有，是你呢？
      * cookie是存在浏览器的。好像明白了，key-value这种形式，key不可以同名，value会被更新掉。但是也会更新的啊，我看token的值没有变
      * 不更新的原因是因为是insert语句而不是update语句吗？有些属性相同的，它就不会再插入了
+     *
      * @param request
      * @return
      */
     @RequestMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie:cookies
-        ) {
-            if (cookie.getName().equals("token")){
-                String token=cookie.getValue();
-                User user=userMapper.findByToken(token);
-                if (user!=null){
-                    request.getSession().setAttribute("user",user);
+        if (cookies != null) {
+            for (Cookie cookie : cookies
+            ) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
             }
         }
         return "index";
